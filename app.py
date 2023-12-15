2e1d8da3b7593028cf8ab60b631afe1e7eaaa3c5
DeVontae <dhudso3@uwo.ca>
2023-12-14 22:09:55 -0500

    store user info using mongodb

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
 import os
-from flask import Flask, redirect, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, redirect, render_template, url_for, jsonify
 from flask_login import UserMixin, login_user,LoginManager, login_required, logout_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
+from wtforms.validators import InputRequired, Length, ValidationError, Email
 from flask_bcrypt import Bcrypt
+from pymongo.mongo_client import MongoClient
+import certifi
+from bson import ObjectId
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-'sqlite:///' + os.path.join(basedir, 'database.db')
-app.config['SECRET_KEY'] = 'thisisasecretkey'
-db = SQLAlchemy(app)
+# secret key needed for csf -> Forms from flask_wtf
+app.config['SECRET_KEY'] = '09dda5ad1019a5899786de3cfd29f0ab'
+
+# used to hash passwords
 bcrypt = Bcrypt(app)
+
+# connect to MongoDb
+password = 'x1pd32hRYY1LH0eu'
+uri = f'mongodb+srv://dhudso3:{password}@loginapp.tjrlnvl.mongodb.net/?retryWrites=true&w=majority'
+
+client = MongoClient(uri, tlsCAFile=certifi.where())
+db = client['loginApp']
+users = db.users
+
+# check if succesful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfuclly connected to MongoDB!")
+except Exception as e:
+    print(e)
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -23,16 +40,49 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    user_data = users.find_one({'_id':ObjectId(user_id)})
+    if user_data:
+        return User(
+            first_name=user_data['first_name'],
+            last_name=user_data['last_name'],
+            email=user_data['email'],
+            usernmae=user_data['username'],
+            password=user_data['password'],
+            _id=user_data['_id']
+        )
+    return None
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    #length set to 80 because password will be hash'd
-    password = db.Column(db.String(80), nullable=False)
-    
+class User:
+    def __init__(self, first_name, last_name, email,username, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.username = username
+        self.password = password
+  
+
+    def save(self):
+        user_data = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email,
+            'username': self.username,
+            'password': self.password
+            
+        }
+        users.insert_one(user_data)
+     
+
+    # Static method for password validation
+    @staticmethod
+    def validate_login(password_hash, password):
+        return bcrypt.check_password_hash(password_hash, password)
+
     
 class RegisterForm(FlaskForm):
+    first_name = StringField(validators=[InputRequired()], render_kw={"placeholder": "First Name"})
+    last_name = StringField(validators=[InputRequired()], render_kw={"placeholder": "Last Name"})
+    email = StringField(validators=[InputRequired(), Email()], render_kw={"placeholder": "Email"})
     username = StringField(validators=[
                            InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
 
@@ -43,8 +93,8 @@ class RegisterForm(FlaskForm):
 
 
     def validate_username(self, username):
-        existing_user_username = User.query.filter_by(
-            username=username.data).first()
+        existing_user_username = users.find_one(
+            {'username':username.data})
         if existing_user_username:
             raise ValidationError(
                 'That username already exists. Please choose a different one.')
@@ -63,15 +113,44 @@ class LoginForm(FlaskForm):
 def home():
     return render_template('home.html')
 
+
+'''
+TESTING INSERT METHOD
+
+@app.route('/test_insert')
+def test_insert():
+    test_user = {
+        'first_name': 'Test',
+        'last_name': 'User',
+        'email': 'test@example.com',
+        'username': 'testuser',
+        'password': 'hashedpassword'
+    }
+    users.insert_one(test_user)
+    return "Test user inserted"
+
+'''
+
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for('dashboard'))
+        user_data =   users.find_one({'username':form.username.data})
+        if user_data and bcrypt.check_password_hash(user_data['password'], form.password.data):
+            user_obj = User(
+                first_name=user_data['first_name'],
+                last_name=user_data['last_name'],
+                email=user_data['email'],
+                username=user_data['username'],
+                password=user_data['password'],
+                _id=user_data['_id']
+            )
+            login_user(user_obj)
+            return redirect(url_for('dashboard'))
+        else:
+            # this is where login failure will be handdled
+            #raiseValidationError
+            pass
     return render_template('login.html', form = form)
 
 
@@ -93,12 +172,33 @@ def register():
     form = RegisterForm()
     
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
+        print("Form validated and submitted successfully.")
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        new_user = User(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            email=form.email.data,
+            username=form.username.data,
+            password=hashed_password
+            )
+            
+        print("New user created:", new_user.first_name, new_user.username)  # Debugging
+
+        new_user.save()
+        print("Save method called for:", new_user.username)  # Confirm save method is called
+
         return redirect(url_for('login'))
-        
+    
+    
+    else:
+        # Debug prints: If this part is reached, the form did not validate
+        print("Form not validated or not submitted.")
+        if form.errors:
+            print("Form errors:")
+            for field, errors in form.errors.items():
+                for error in errors:
+                    print(f"Error in the {field} field - {error}")
+  
     return render_template('register.html', form = form)
     
 

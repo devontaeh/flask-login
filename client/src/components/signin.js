11c4f0776e025a34d034bd2b481f956faab96a9c
DeVontae <dhudso3@uwo.ca>
2024-01-02 17:43:33 -0500

    changed signin form to use username instead of email

--- client/src/components/signin.js
+++ client/src/components/signin.js
@@ -35,7 +35,7 @@ export default function SignIn() {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
+      username: data.get('username'),
       password: data.get('password'),
     });
   };
@@ -63,10 +63,10 @@ export default function SignIn() {
               margin="normal"
               required
               fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
               autoFocus
             />
             <TextField
